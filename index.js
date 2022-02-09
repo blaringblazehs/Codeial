115a13095a52a5f2bc304b0662457c6f608280d2
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2022-02-10 00:07:55 +0530

    added routs

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//use express router
+app.use("/", require("./routes"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error running server ${err}`);
