06d1785dd195a911a72725f8d6c8934d706a82d4
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2022-03-02 19:43:03 +0530

    error in comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,12 @@ const Post = require("../models/post");
 module.exports.home = function (req, res) {
     Post.find({})
         .populate("user")
+        .populate({
+            path: "comments",
+            populate: {
+                path: "user",
+            },
+        })
         .exec(function (err, posts) {
             return res.render("home", {
                 title: "Codeial | Home",
