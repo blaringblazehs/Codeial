daa084cc081bac27a5691bc31a7dab7a7e6b6d5a
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2022-03-07 05:25:03 +0530

    Converted to AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,10 +2,18 @@ const Post = require("../models/post");
 const Comment = require("../models/comment");
 module.exports.create = async function (req, res) {
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id,
         });
+        if (req.xhr) {
+            return res.status(200).json({
+                data: {
+                    post: post,
+                },
+                message: "Post created",
+            });
+        }
         req.flash("success", "Post Published!");
         return res.redirect("back");
     } catch (err) {
@@ -19,6 +27,14 @@ module.exports.destroy = async function (req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
+            if (req.xhr) {
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id,
+                    },
+                    message: "post deleted",
+                });
+            }
             req.flash("success", "Post deleted successfully!");
             return res.redirect("back");
         } else {
@@ -26,7 +42,8 @@ module.exports.destroy = async function (req, res) {
             res.redirect("back");
         }
     } catch (err) {
-        res.flash("error", err);
+        req.flash("error", err);
+        console.log(err);
         return res.redirect("back");
     }
 };
